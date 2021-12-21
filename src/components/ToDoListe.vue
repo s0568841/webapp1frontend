45f60ee3c632f17c40a4ec2a194b1e54158201ef
s0568841 <s0568841@htw-berlin.de>
2021-12-21 16:05:21 +0100

    Hinzufügen von fetch zur verbindung mit dem Backend

---
+++ src/components/ToDoListe.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="col" v-for="ToDoListe in ToDoListe" :key="ToDoListe.id">
+    <div class="card text-center">
+      <div class="card-header">
+        To-Do-Liste
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">{{ToDoListe.headline}}</h5>
+        <p class="card-text">{{ToDoListe.note}}</p>
+        <a href="#" class="btn btn-primary">erstelle Eintrag</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ToDoListe',
+  data () {
+    return {
+      ToDoListe: []
+    }
+  },
+  mounted () {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    }
+    fetch('https://localhost:8080/entity/ToDoListeEntity', requestOptions)
+      .then(response => response.json())
+      .then(result => result.forEach(ToDoListe => {
+        this.ToDoListe.push(ToDoListe)
+      }))
+      .catch(error => console.log('error', error))
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Hello World"/>
-  </div>
+  <ToDoListe></ToDoListe>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import ToDoListe from '@/components/ToDoListe'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    ToDoListe
   }
 }
 </script>
